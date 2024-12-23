7a47544ffaa0d363e9a9e83773b7095c919bc59f
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2024-12-23 13:23:32 -0500

    Add files via upload

---
+++ assets/preload/data/420/smoke.lua
@@ -0,0 +1,8 @@
+function onCreatePost()
+    print("2:test!!!!")
+    
+    makeLuaSprite('', 'smoke', -160, 0)
+    setScrollFactor('', 0.2, 0.2)
+    setObjectCamera('', 'game')
+    addLuaSprite('', true)
+end
