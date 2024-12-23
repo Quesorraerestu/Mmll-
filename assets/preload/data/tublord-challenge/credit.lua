afc0390c8612869eed0bae947fa7dfd04a1073b4
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2024-12-23 16:32:01 -0500

    Add files via upload

---
+++ assets/preload/data/tublord-challenge/credit.lua
@@ -0,0 +1,49 @@
+local credit = "TREATMENT"
+local duration = 2
+local delay = 0
+
+--
+
+function onSongStart()
+	makeLuaSprite("credit", "credits/"..credit, 0,250)
+	setProperty("credit.x", -getProperty("credit.width"))
+	setObjectCamera("credit", "hud")
+
+    runTimer("delay", delay)
+end
+
+function onTweenCompleted(tag)
+    if tag == "credit" then
+        runTimer("credit", 2.5)
+    end
+end
+
+function onTimerCompleted(tag)
+    if tag == "delay" then
+
+        doTweenX("credit", "credit", 0, duration, "quadinout")
+    elseif tag == "credit" then
+        doTweenX("credit", "credit", -getProperty("credit.width"), duration, "quadinout")
+    end
+end
+
+function onCreatePost()
+	makeLuaText("nigger", "TUBLORD CHALLENGE :", 600, 760, 20)
+	addLuaText("nigger")
+	setTextSize("nigger", 25)
+	setTextAlignment("nigger", 'left')
+	makeLuaText("art", "ART BY SKUMZ", 600, 1050, 10)
+	addLuaText("art")
+	setTextSize("art", 15)
+	setTextAlignment("art", 'left')
+	makeLuaText("song", "SONG BY ANGELPLAY3", 600,  1050, 25)
+	addLuaText("song")
+	setTextSize("song", 15)
+	setTextAlignment("song", 'left')
+	makeLuaText("chart", "CHART BY SKUMZ", 600,  1050, 40)
+	addLuaText("chart")
+	setTextSize("chart", 15)
+	setTextAlignment("chart", 'left')
+end
+
+--toads big
---
+++ assets/preload/data/tublord-challenge/death.lua
@@ -0,0 +1,6 @@
+function onCreate()
+setPropertyFromClass('GameOverSubstate', 'characterName', 'bf'); --Character json file for the death animation
+setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
+setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'tublordChallenge/gameOver_tublord'); --put in mods/music/
+setPropertyFromClass('GameOverSubstate', 'endSoundName', 'tublordChallenge/gameOverEnd_tublord'); --put in mods/music/
+end
