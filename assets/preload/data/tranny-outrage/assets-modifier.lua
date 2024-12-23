ba67baef1a8d78302be8228b22c8a07a214b0fce
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2024-12-23 16:21:24 -0500

    Add files via upload

---
+++ assets/preload/data/tranny-outrage/assets-modifier.lua
@@ -0,0 +1,7 @@
+function init()
+    runHaxeCode([[
+        game.assets.healthBar = "healthBar";
+        game.assets.scoreTxtBg = "scoreTxtBg";
+        game.assets.NOTE_assets = "NOTE_assets";
+    ]])
+end
---
+++ assets/preload/data/tranny-outrage/death.lua
@@ -0,0 +1,6 @@
+function onCreate()
+setPropertyFromClass('GameOverSubstate', 'characterName', 'william-dead1'); --Character json file for the death animation
+setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
+setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gmto'); --put in mods/music/
+setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
+end
