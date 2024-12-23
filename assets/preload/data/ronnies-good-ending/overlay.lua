07a2a7c43852da27819d393460799ae0c1186b51
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2024-12-23 16:13:48 -0500

    Add files via upload

---
+++ assets/preload/data/ronnies-good-ending/overlay.lua
@@ -0,0 +1,9 @@
+function onCreatePost()
+    print("2:test!!!!")
+    
+    makeLuaSprite('', 'ge', -75, -40)
+    setScrollFactor('', 0, 0)
+    setObjectCamera('', 'game')
+    setGraphicSize('',  1450, 800)
+    addLuaSprite('', true)
+end
